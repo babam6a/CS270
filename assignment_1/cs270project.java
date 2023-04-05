15cc1ac7702cf6ebc01f9feb43aa8a344159870a
jinnkim0112 <71919489+jinnkim0112@users.noreply.github.com>
2023-04-05 10:40:46 +0900

    update the cs270 project
    Now, motor parameters must be changed and sensor bug must be considered

--- assignment_1/cs270project.java
+++ assignment_1/cs270project.java
@@ -8,20 +8,21 @@
  * 
  * Functions:
  * main() : motorMoveForward() -> color() -> IRSensor()
- * private void motorMoveForward() : moves ev3 forward for certain time(precise changes must be made)
+ * private void motorMoveForward() : moves ev3 forward for certain time(changes to parameters must be made)
  * private void color() : reads color and motorRotateRight() when color_id < 9, motorRotateLeft() otherwise
  * public static void IRSensor() : If > 23cm print "Box not ahead" if not, print "Box ahead"
- * motorRotateRight() : rotate right(by rotating up the left motor) for certain time(precise changes must be made)
- * motorRotateLeft() : rotate left(by rotating up the right motor) for certain time(precise changes must be made)
+ * motorMoveBackward() : ev3 moves back before each rotation for precise positioning(changes to parameters must be made)
+ * motorRotateRight() : rotate right(by rotating up the left motor) for certain time(changes to parameters must be made)
+ * motorRotateLeft() : rotate left(by rotating up the right motor) for certain time(changes to parameters must be made)
+ * test() : function to print out strings on the lcd pannel for testing
+ * 
+ * when rotating, make ev3 goes back and rotates for it to be on spot for the next motorMoveForward() move
+ * -> be careful for edge case when ev3 is surrounded by two boxes(it might move the box when going back for rotation)
  * 
  * To-do:
- * make a new function test() to test the sensors by printing out the variables on the lcd(would not use in practice)
- * take variables in individual sensors to a global variable to stop time loss from defining the same variable again
- * fix bug where the ev3 tilts to the right when going straight
+ * fix bug where the ev3 tilts a little when going straight
  * when going straight, make ev3 go exactly one block, or 23cm
  * when rotating, make ev3 rotate precisely 90 degrees
- * when rotating, make ev3 go back and rotate for it to be on spot for the next turn
- * -> be careful for edge case when ev3 is surrounded by two boxes(it might move the box when going back for rotation)
  * find a method to account for false outcomes on the EV3ColorSensor
  */
 
@@ -41,83 +42,93 @@ import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class cs270project {
-	private static EV3IRSensor sensor;
-	private static EV3ColorSensor color_sensor;
+	private final boolean testing = true; // change this value to toggle on/off the test function
+	private static EV3IRSensor sensor = new EV3IRSensor(SensorPort. S1);
+	private static EV3ColorSensor color_sensor = new EV3ColorSensor(SensorPort. S2);
 	EV3 ev3 = (EV3) BrickFinder.getLocal();
 	TextLCD lcd = ev3.getTextLCD();
 	
-	private void color(){
-		color_sensor = new EV3ColorSensor(SensorPort. S2);
-	
-		String str = "";
-		int color_id = color_sensor.getColorID();
-		
-		switch(color_id)
-		{
-		case Color.BLACK:
-			str = "Color.Black = "+String.valueOf(color_id);
-			break;
-		case Color.BLUE:
-			str = "Color.Blue = "+String.valueOf(color_id);
-			break;
-		case Color.BROWN:
-			str = "Color.Brown = "+String.valueOf(color_id);
-			break;
-		case Color.CYAN:
-			str = "Color.Cyan = "+String.valueOf(color_id);
-			break;
-		case Color.DARK_GRAY:
-			str = "Color.Dark_Gray = "+String.valueOf(color_id);
-			break;
-		case Color.GRAY:
-			str = "Color.Gray = "+String.valueOf(color_id);
-			break;
-		case Color.GREEN:
-			str = "Color.Green = "+String.valueOf(color_id);
-			break;
-		case Color.LIGHT_GRAY:
-			str = "Color.Light_Gray = "+String.valueOf(color_id);
-			break;
-		case Color.MAGENTA:
-			str = "Color.Magenta = "+String.valueOf(color_id);
-			break;
-		case Color.NONE:
-			str = "Color.None = "+String.valueOf(color_id);
-			break;
-		case Color.ORANGE:
-			str = "Color.Orange = "+String.valueOf(color_id);
-			break;
-		case Color.PINK:
-			str = "Color.Pink = "+String.valueOf(color_id);
-			break;
-		case Color.RED:
-			str = "Color.Red = "+String.valueOf(color_id);
-			break;
-		case Color.WHITE:
-			str = "Color.White = "+String.valueOf(color_id);
-			break;
-		case Color.YELLOW:
-			str = "Color.Yellow = "+String.valueOf(color_id);
-			break;
-		}
+	private void test(int testNum, float result){
 		lcd.clear();
-		lcd.drawString(str, 1, 4);
-		Delay.msDelay(10000);
-		if(color_id == Color.RED) motorRotateRight();
-		else motorRotateLeft();
+		String str = "";
+		
+		/* test for function color() */
+		if(testNum == 1) {
+			int color_id = (int) result;
+			switch(color_id)
+			{
+			case Color.BLACK:
+				str = "Color.Black = "+String.valueOf(color_id);
+				break;
+			case Color.BLUE:
+				str = "Color.Blue = "+String.valueOf(color_id);
+				break;
+			case Color.BROWN:
+				str = "Color.Brown = "+String.valueOf(color_id);
+				break;
+			case Color.CYAN:
+				str = "Color.Cyan = "+String.valueOf(color_id);
+				break;
+			case Color.DARK_GRAY:
+				str = "Color.Dark_Gray = "+String.valueOf(color_id);
+				break;
+			case Color.GRAY:
+				str = "Color.Gray = "+String.valueOf(color_id);
+				break;
+			case Color.GREEN:
+				str = "Color.Green = "+String.valueOf(color_id);
+				break;
+			case Color.LIGHT_GRAY:
+				str = "Color.Light_Gray = "+String.valueOf(color_id);
+				break;
+			case Color.MAGENTA:
+				str = "Color.Magenta = "+String.valueOf(color_id);
+				break;
+			case Color.NONE:
+				str = "Color.None = "+String.valueOf(color_id);
+				break;
+			case Color.ORANGE:
+				str = "Color.Orange = "+String.valueOf(color_id);
+				break;
+			case Color.PINK:
+				str = "Color.Pink = "+String.valueOf(color_id);
+				break;
+			case Color.RED:
+				str = "Color.Red = "+String.valueOf(color_id);
+				break;
+			case Color.WHITE:
+				str = "Color.White = "+String.valueOf(color_id);
+				break;
+			case Color.YELLOW:
+				str = "Color.Yellow = "+String.valueOf(color_id);
+				break;
+			}
+			lcd.drawString(str, 1, 4);
+		}
+		
+		/* test for function IRSensor() */
+		if(testNum == 2){
+			if(result >= 23) str = "no box ahead";
+			else str = "box ahead";
+			lcd.drawString(str, 1, 4);
+			str = "distance : " + result;
+			lcd.drawString(str, 2, 4);
+		}
+	}
+	
+	private void color(){
+		int color_id = color_sensor.getColorID();
+		if(testing) test(1, color_id);
 	}
 	
 	private boolean IRSensor(){
-		sensor = new EV3IRSensor(SensorPort. S1);
 		
 		SampleProvider distanceMode = sensor.getDistanceMode();
 		float value[] = new float[distanceMode.sampleSize()];
-		Delay.msDelay(30000);
 		distanceMode.fetchSample(value, 0);
 		float centimeter = value[0];
-		lcd.clear();
-		if(centimeter < 23)lcd.drawString("box ahead : "+centimeter, 1, 4);
-		else lcd.drawString("box not ahead : "+centimeter, 1, 4);
+		
+		if(testing) test(2, centimeter); // test number for IRSensor is 2
 		Delay.msDelay(30000);
 		return centimeter < 1;
 	}
@@ -127,22 +138,53 @@ public class cs270project {
 		RegulatedMotor rightMotor = Motor. B;
 		
 		leftMotor.setSpeed(400);
-		rightMotor.setSpeed(400);
+		rightMotor.setSpeed(371);
 		leftMotor.setAcceleration(800);
-		rightMotor.setAcceleration(800);
+		rightMotor.setAcceleration(742);
 		
 		leftMotor.forward();
 		rightMotor.forward();
-		
+
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1600);
 		}catch(InterruptedException e) {}
-		
 		leftMotor.stop();
+		rightMotor.stop();
+		
+		rightMotor.backward();
+		try {
+			Thread.sleep(620);
+		}catch(InterruptedException e) {}
+		rightMotor.stop();
+	}
+
+	public static void motorMoveBackward() {
+		RegulatedMotor leftMotor = Motor. A;
+		RegulatedMotor rightMotor = Motor. B;
+		
+		leftMotor.setSpeed(400);
+		rightMotor.setSpeed(371);
+		leftMotor.setAcceleration(800);
+		rightMotor.setAcceleration(742);
+		
+		leftMotor.backward();
+		rightMotor.backward();
+
+		try {
+			Thread.sleep(1000);
+		}catch(InterruptedException e) {}
+		leftMotor.stop();
+		rightMotor.stop();
+		
+		rightMotor.forward();
+		try {
+			Thread.sleep(620);
+		}catch(InterruptedException e) {}
 		rightMotor.stop();
 	}
 	
 	public static void motorRotateLeft() {
+		motorMoveBackward();
 		RegulatedMotor rightMotor = Motor. B;
 		
 		rightMotor.setSpeed(400);
@@ -151,13 +193,14 @@ public class cs270project {
 		rightMotor.forward();
 		
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1690);
 		}catch(InterruptedException e) {}
 		
 		rightMotor.stop();
 	}
 
 	public static void motorRotateRight() {
+		motorMoveBackward();
 		RegulatedMotor leftMotor = Motor. A;
 		
 		leftMotor.setSpeed(400);
@@ -166,7 +209,7 @@ public class cs270project {
 		leftMotor.forward();
 		
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1690);
 		}catch(InterruptedException e) {}
 		
 		leftMotor.stop();
@@ -175,6 +218,8 @@ public class cs270project {
 	public static void main(String[] args) {
 		cs270project project = new cs270project();
 		motorMoveForward();
+		motorRotateRight();
+		motorRotateLeft();
 		project.color();
 		project.IRSensor();
 	}
