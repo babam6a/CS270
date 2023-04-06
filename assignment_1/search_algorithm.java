c80e9bddb70ba088a041c8ba20aa576e400a7c9d
babam6a <babamba@kaist.ac.kr>
2023-04-06 20:54:21 +0900

    modulate functions to implement EV3_related code

--- assignment_1/search_algorithm.java
+++ assignment_1/search_algorithm.java
@@ -5,32 +5,68 @@ import java.util.Scanner;
 import java.util.Collections;
 
 public class search_algorithm {
-	public static int left_turn(int direction) {
-        switch (direction) {
-            case 0: // up
-                return 3;
-            case 1: // right
-                return 0;
-            case 2: // down
-                return 1;
-            case 3: // left
-                return 2;
-        }
-        return -1;
+	// #########################################################
+	// should implement robot-related function from here...
+
+	public static void move_forward() {
+		// code will be inserted here
+		return;
+	}
+	public static void left_turn() {
+		// code will be inserted here
+        return;
     }
-    public static int right_turn(int direction) {
-        switch (direction) {
-            case 0: // up
-                return 1;
-            case 1: // right
-                return 2;
-            case 2: // down
-                return 3;
-            case 3: // left
-                return 0;
-        }
-        return -1;
+    public static void right_turn() {
+		// code will be inserted here
+        return;
     }
+
+	public static boolean is_box(int[][] map, int[] position) {
+		if (map[position[0]][position[1]] == 2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean is_red(int[][] map, int[] position) {
+		if (map[position[0]][position[1]] == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean select_start_point() {
+		Scanner scan = new Scanner(System.in);
+		boolean result;
+
+		System.out.println("Select where to start. (0,0): 0, (5,3): 1");
+		int start_point = scan.nextInt();
+		if (start_point == 0) {
+			result = true;
+		} else {
+			result = false;
+		}
+		scan.close();
+		return result;
+	}
+	// to here.
+	// #########################################################
+
+	public static void turn_robot(int curr_direction, int target_direction) {
+		int diff = target_direction - curr_direction;
+		if (diff < 0) {
+			for (int i = 0; i < Math.abs(diff); i++) {
+				left_turn();
+			}
+		}
+		else {
+			for (int i = 0; i < Math.abs(diff); i++) {
+				right_turn();
+			}
+		}
+	}
     
     public static ArrayList<Integer> find_path(int[] current_pos, int[] target_pos) {
         int dx = target_pos[0] - current_pos[0];
@@ -90,38 +126,28 @@ public class search_algorithm {
             return true;
         }
     }
-
-	public static boolean is_box(int[][] map, int[] position) {
-		if (map[position[0]][position[1]] == 2) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public static boolean is_red(int[][] map, int[] position) {
-		if (map[position[0]][position[1]] == 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
     
     public static void main(String[] args) {
-        int[][] drdc = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+		int[][] drdc = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 		boolean[][] visited = new boolean[6][4];
-		int[] original_pos = {0,0};
+		int[] original_pos;
+		if (select_start_point()) {
+			original_pos = new int[] { 0, 0 };
+		} else {
+			original_pos = new int[] { 5, 3 };
+		}
 		int[] current_pos = original_pos;
 		
-	// #########################################################
+	// s #########################################################
 		// for debugging. not used in real test
-		// nothing = 0, red = 1, box = 2
 		int[][] map = new int[6][4]; //x, y
 		int[][] red = {{0,2},{5,1}};
 		int[][] box = {{2,0},{4,3}};
 		int move_count = 0;
 		int turn_count = 0;
-		
+
+
+		// nothing = 0, red = 1, box = 2
 		for (int i = 0; i < red.length; i++) {
 			int[] temp = red[i];
 			map[temp[0]][temp[1]] = 1;
@@ -130,7 +156,7 @@ public class search_algorithm {
 			int[] temp = box[i];
 			map[temp[0]][temp[1]] = 2;
 		}
-	// #########################################################
+	// e #########################################################
 		
 		// answer
 		ArrayList<int[]> red_found = new ArrayList<int[]>();
@@ -162,7 +188,9 @@ public class search_algorithm {
             		path = find_path(current_pos, target_pos);
         		}
         		
+				// s ###################################
         		System.out.printf("current: %s, target: %s, path: %s\n",Arrays.toString(current_pos),Arrays.toString(target_pos), path.toString());
+				// e ###################################
         		
         		//move along with calculated path
         		for (int way: path) {
@@ -176,8 +204,13 @@ public class search_algorithm {
         			}
         			
 					if (way != direction) {
+						turn_robot(direction, way);
+					// s ###################################
+						direction = way;
 						turn_count++;
+					// e ###################################
 					}
+
         			int[] d = drdc[way];
     			    int[] new_pos = {current_pos[0] + d[0], current_pos[1] + d[1]};
     			    
@@ -188,21 +221,32 @@ public class search_algorithm {
     			        }
     			        
     			        while (!is_right_position(new_pos) || is_box(map, new_pos)) { // turn until no box in front of robot
-        					way = (way + 1) % 4; //turn (up -> right), (down -> left)
+        					direction = (direction + 1) % 4; //turn right
+							right_turn();
+						// s ###################################
 							turn_count++;
+						// e ###################################
         					//recalculate
-        					d = drdc[way];
+        					d = drdc[direction];
         					new_pos = new int[] {current_pos[0] + d[0], current_pos[1] + d[1]};
     			        }
-    			        current_pos = new_pos;
+
+						move_forward();
+					// s ###################################
+						current_pos = new_pos;
 						move_count++;
     			        System.out.println("move_to: " + Arrays.toString(current_pos) + "\n");
+					// e ###################################
     			        visited[new_pos[0]][new_pos[1]] = true;
     			        break; // recalculate path
+
         		    } else {
-        		        current_pos = new_pos;
+						move_forward();
+					// s ###################################
+						current_pos = new_pos;
 						move_count++;
 						System.out.println("move_to: " + Arrays.toString(current_pos) + "\n");
+					// e ###################################
         		    }
         		}
     		}
@@ -212,7 +256,8 @@ public class search_algorithm {
     		}
 		}
 		
-		
+		// s ###################################
+		// print function should be different
 		for (int[] item: red_found) {
 		    System.out.println("red_found: " + Arrays.toString(item) + "\n");
 		}
@@ -221,5 +266,6 @@ public class search_algorithm {
 		}
 		System.out.printf("move_count: %d, turn_count: %d\n",move_count, turn_count);
 		System.out.printf("current_pos: %s\n", Arrays.toString(current_pos));
+		// e ###################################
     }
 }
