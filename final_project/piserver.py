af76e16342607fa5da4002a6ab4629958fe352ec
babam6a <65668742+babam6a@users.noreply.github.com>
2023-05-23 22:57:59 +0900

    Update piserver.py

--- final_project/piserver.py
+++ final_project/piserver.py
@@ -1,28 +1,38 @@
 from camera import take_picture
-from trajectory import *
+from trajectory import cal_z_angle, cal_trajectory
 import socket
+import struct
 
 def model(picture) :
-    return (0,0)
+    ret = []
+    return ret
 
 def make_data() :
-    d = 0 # distance from target
+    d = 30 # distance from target
     h_init = 0 # height of model
     g = 9.8 # gravity
     max_v = 30 # maximum strength of archery
     file_name ='capture.jpg' # file_name of capture image
     
     picture = take_picture(file_name)
-    (x,y) = model(picture) # CNN model
+    cord_list = model(picture) # CNN model
+
+    ans_str = ""
+
+    if len(cord_list) == 0 :
+        ans_str = "Nothing"
     
-    z_angle = cal_z_angle(d,x)
-    (x_angle, v) = cal_trajectory(d, z_angle, h_init, g, y, max_v)
-    
-    if (x_angle, v) == (0,0) :
-        return "Impossible target" # cannot shoot
     else :
-        return "%.3f %.3f %.3f"%(z_angle, x_angle, v)
+        for (x, y) in cord_list :
+            z_angle = cal_z_angle(d,x)
+            (x_angle, v) = cal_trajectory(d, z_angle, h_init, g, y, max_v)
     
+            if (x_angle, v) == (0,0) :
+                ans_str +=  "Impossible " # cannot shoot
+            else :
+                ans_str += "%.3f/%.3f/%.3f "%(z_angle, x_angle, v)
+    
+    return ans_str.rstrip(" ")
 
 if __name__ == "__main__" :
     server_address = "10.0.1.3"
@@ -30,6 +40,7 @@ if __name__ == "__main__" :
     size = 1024
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
     sock.bind((server_address, port))
     sock.listen(1)
 
@@ -40,11 +51,22 @@ if __name__ == "__main__" :
         print("Connected client", clientInfo)
         
         while True:
-            data = client.recv(size)
-            if data == "Client ready" :
-                send_data = make_data()
-                client.send(send_data)
-            elif data == "Client finished" :
+            origin_data = b""
+            while True :
+                origin_data += client.recv(size)
+                if b"\n" in origin_data :
+                    break
+
+            data = origin_data.decode().rstrip("\n")
+            print("data: %s"%data)
+
+            if "Client ready" in data:
+                data = make_data()
+                data = bytearray(data, "utf8")
+                size = len(data)
+                client.sendall(struct.pack("!H", size))
+                client.sendall(data)
+            elif "Client finished" in data :
                 print("Finish program")
                 client.close()
                 sock.close()
