24040af3a25c32806629f4dd97374faced4def3a
jinnkim0112 <71919489+jinnkim0112@users.noreply.github.com>
2023-06-01 13:27:40 +0900

    added model to piserver.py
    please pip install roboflow and compile the code in access to the internet

--- final_project/piserver.py
+++ final_project/piserver.py
@@ -1,7 +1,21 @@
 from camera import take_picture
 from trajectory import cal_z_angle, cal_trajectory
+import cv2 as cv
+from roboflow import Roboflow
 import socket
 import struct
+
+# need internet access to load the model
+# 2 projects with 3 models each -> a total of 6 models
+# pick the one that suits best
+
+apiKey = ["bzK1X042pq2SWqOQS9t0", "VPr4wYYv2Q7tB49pzRqA"][1] # 2 projects available
+version = [1,2,3][2] # both project has total of 3 versions
+
+rf = Roboflow(api_key=apiKey)
+projectName = "cs270-team-8"+ ("-mkjrf" if ("VPr4wYYv2Q7tB49pzRqA" == apiKey) else "")
+project = rf.workspace().project(projectName)
+model = project.version(version).model
 
 d = 30 # distance from target
 h_init = 0 # height of model
@@ -9,12 +23,20 @@ g = 9.8 # gravity
 max_v = 30 # maximum strength of archery
 
 def model(picture) :
+    # when confidence is low, more predictions appear making it likely to predict false results
+    # when confidence is high, less predictions appear making it likely to pick out true results
+    # overall, picking the right confidence is essential for prediction
+    result = model.predict(picture, confidence=30, overlap=30).json()
+
     ret = []
+    for i in range(len(result['predictions'])):
+        ret.append( (int(result['predictions'][i]['x'], int(result['predictions'][i]['y'])) )
+
     return ret
 
 def make_data() :
     file_name ='capture.jpg' # file_name of capture image
-    
+
     picture = take_picture(file_name)
     cord_list = model(picture) # CNN model
 
@@ -22,17 +44,17 @@ def make_data() :
 
     if len(cord_list) == 0 :
         ans_str = "Nothing"
-    
+
     else :
         for (x, y) in cord_list :
             z_angle = cal_z_angle(d,x)
             (x_angle, v) = cal_trajectory(d, z_angle, h_init, g, y, max_v)
-    
+
             if (x_angle, v) == (0,0) :
                 ans_str +=  "Impossible " # cannot shoot
             else :
                 ans_str += "%.3f/%.3f/%.3f "%(z_angle, x_angle, v)
-    
+
     return ans_str.rstrip(" ")
 
 def position_check() :
@@ -69,7 +91,7 @@ if __name__ == "__main__" :
     size = 1024
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((server_address, port))
     sock.listen(1)
 
@@ -78,7 +100,7 @@ if __name__ == "__main__" :
     try:
         client, clientInfo = sock.accept()
         print("Connected client", clientInfo)
-        
+
         while True:
             origin_data = b""
             while True :
@@ -95,14 +117,14 @@ if __name__ == "__main__" :
                 size = len(data)
                 client.sendall(struct.pack("!H", size))
                 client.sendall(data)
-                
+
             elif "Position check" in data :
                 send = position_check()
                 send = bytearray(send, "utf8")
                 size = len(send)
                 client.sendall(struct.pack("!H", size))
                 client.sendall(send)
-                    
+
             elif "Client finished" in data :
                 print("Finish program")
                 client.close()
